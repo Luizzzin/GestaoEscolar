28a80bf59fd8570d93c69cf7ab67e6c8c83290bc
Luizzzin <luizhenribarbodias@gmail.com>
2025-03-29 11:51:57 -0300

    feat:code professor

--- src/models/Professor.java
+++ src/models/Professor.java
@@ -1,4 +1,28 @@
 package models;
 
 public class Professor {
+
+    private String nomeProfessor;
+    private String disciplinaProfessor;
+
+    public Professor(String nomeProfessor, String disciplinaProfessor) {
+        this.nomeProfessor = nomeProfessor;
+        this.disciplinaProfessor = disciplinaProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public String getDisciplinaProfessor() {
+        return disciplinaProfessor;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nomeProfessor='" + nomeProfessor + '\'' +
+                ", disciplinaProfessor='" + disciplinaProfessor + '\'' +
+                '}';
+    }
 }
